110cc05cf497a2d67bc847e2e98392d471458730
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-12 16:51:53 -0300

    (AuthPages): Data type: String

---
+++ Data Types and Logical Operators/4.String/script.js
@@ -0,0 +1,9 @@
+console.log(typeof "Heya, My name is Guilherme! ");
+
+console.log(typeof 'At the moment, I am studying JavaScript!');
+
+console.log(typeof `Wassup`);
+
+console.log(typeof "Infinity");
+
+console.log(typeof Infinity);
