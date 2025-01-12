b917bc99fc50ac8f9b1bdc7e479e1bd80ea36ba4
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-12 16:32:16 -0300

    (AuthPages): Learning Arithmetic operations with number.

---
+++ Data Types and Logical Operators/2.Arithmetic operations with number/script.js
@@ -0,0 +1,14 @@
+
+console.log(typeof(2 + 3));
+
+console.log(typeof(5 - 12));
+
+console.log(typeof( 4 * 4));
+
+console.log(typeof(15 / 3));
+
+console.log(typeof(5 + (5 * 4)));
+
+console.log(typeof(21 % 2));
+
+console.log(typeof(20 % 2));
