1611b7d35245b926526616bb146f1ceb7a82b9f3
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-12 17:20:09 -0300

    (AuthPages): Technical details about String

---
+++ Data Types and Logical Operators/5.Technical details about String/script.js
@@ -0,0 +1,9 @@
+console.log("Eu vos dou um novo mandamento: amai-vos uns aos outros. \nComo eu vos amei, assim também vós deveis amar-vos uns aos outros. ");
+
+console.log("My name is 'Guilherme'.");
+
+console.log('My name is "Isabela".');
+
+console.log(`What is the multiplication of 25 * 3 ? = ${25 * 3}`);
+
+console.log("The " + "Flat " + "Earth");
