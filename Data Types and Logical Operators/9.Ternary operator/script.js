539fede033a6f669d3e835e40619855616c95ca4
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-13 10:48:44 -0300

    (AuthPages): Learning about Ternary Operator

---
+++ Data Types and Logical Operators/9.Ternary operator/script.js
@@ -0,0 +1,8 @@
+
+// condition ? expressionIfTrue : expressionIfFalse;
+
+console.log(5 > 2 ? "É sim" : "É não");
+
+console.log(false ? 5 : 4);
+
+console.log("Guilherme" == "Guilherme" ? "Bom dia, Deus Abençoe seu dia Guilherme!" : "Bom dia , não e o Guilherme.");
