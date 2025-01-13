1a2d8119f56992062b8fa9c0bad9c11720d44b8c
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-13 10:33:58 -0300

    (AuthPages): Logical Operators - Pratice and Theory

---
+++ Data Types and Logical Operators/8.Logical Operators - Theory/script.js
@@ -0,0 +1,10 @@
+
+console.log( 5 > 3 && 3 == 2);
+
+console.log(5 > 3 || 3 == 1);
+
+console.log("Guilherme" == "Guilherme" && 28 == 28);
+
+console.log("Guilherme" == "Gui" || false);
+
+console.log(!(!(true && true)))
