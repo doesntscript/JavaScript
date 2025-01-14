ac87aa31cd8a5cc68cfffb20b16ae16c36429821
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-14 09:12:25 -0300

    (AuthPages): Exercise Data Types - 04

--- Data Types and Logical Operators/12.Exercises about data types/03/script.js
+++ Data Types and Logical Operators/12.Exercises about data types/03/script.js
@@ -1,7 +1,7 @@
 
 
-const idade = 2005;  
-const idade2 = 2004
+const idade = 19;  
+const idade2 = 19;
 
 console.log( idade > idade2 );
 
@@ -9,4 +9,4 @@ console.log(idade2 < idade);
 
 console.log(idade == idade2);
 
-console.log(idade != '2005');
+console.log(idade != '19');
---
+++ Data Types and Logical Operators/12.Exercises about data types/04/script.js
@@ -0,0 +1,11 @@
+
+// and &&
+console.log("Sophie" == "Sophie" && (5 == 5));
+
+// ! not
+console.log(!(5 > 6));
+
+// || or
+const idade =  19;
+const idade2 = 20;
+console.log(idade >= idade2 || (5 == 5));
