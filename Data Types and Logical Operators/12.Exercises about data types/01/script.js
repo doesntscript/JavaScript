4c6a5b70412dd4af9814e4f4f0cc98b202f37fbd
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-01-14 08:12:03 -0300

    (AuthPages): Exercise about Data Types - 01

---
+++ Data Types and Logical Operators/12.Exercises about data types/01/script.js
@@ -0,0 +1,15 @@
+
+console.log("Good Morning , I'm staterd mine exercise about JavaScript data types right now. ");
+
+console.log('Today I did already to the gym trained biceps.');
+
+console.log(`Wassup, How is it goin today ? `);
+
+// Template literals são strings delimitadas por crases (``) e permitem:
+
+// Interpolação de expressões com ${expression}.
+
+// Exemplo de interpolação :
+
+const name = "Guilherme Cosme dos Santos";
+console.log(`Wassup, ${name}, how is it goin today?`);
